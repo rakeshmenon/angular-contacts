d09d0e1ad129c19f5280ee0874c94e2b51eec45f
Rakesh Menon <menonrakesh@live.com>
2012-09-29 15:01:49 +0530

    Refresh button added for dynamically fetching contacts

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,12 +3,20 @@
 /* Controllers */
 
 var ContactControl = function($scope, $http) {
-  $http.get('contacts/contacts.json').success(function (data) {
-    $scope.contacts = data;
-    window.localStorage.setItem("data", JSON.stringify(data));
-  }).error(function (e) {
-  	$scope.contacts = JSON.parse(window.localStorage.getItem("data"));
-  });
+  var updateContacts = function () {
+    $http.get('contacts/contacts.json').success(function (data) {
+        $scope.contacts = data;
+        window.localStorage.setItem("data", JSON.stringify(data));
+    }).error(function (e) {
+  	    $scope.contacts = JSON.parse(window.localStorage.getItem("data"));
+    });
+  };
+
+  updateContacts();
+
+  $scope.update = function () {
+    updateContacts();
+  };
 
   $scope.sortOrder = 'id';
 }
